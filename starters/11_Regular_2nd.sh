9313cf476b208101d8fe98050a61c28d9760bad3
Volker Strobel <volker.strobel87@gmail.com>
2019-10-25 16:46:46 +0200

    added starters

---
+++ starters/11_Regular_2nd.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. ./global_config.sh
+
+EXPERIMENT='9_Regular'
+
+# Decision rule 1: LAC
+# Decision rule 2: W-MSR
+DECISIONRULE=$1
+PERCENT_BLACKS=(0 10 20 30 40 50 60 70 80 90 100)
+EPSILONS=(0.01)
+NUMBYZANTINE=(0)
+LENGTHOFRUNS=(1000)
+# An explanation of the Byzantine styles can be found in global config
+
+DETERMINECONSENSUS="false"
+BYZANTINESWARMSTYLES=(0)
+MIXINGS=(4) # mix or tiles or just have a binary field
+SUBSWARMCONSENSUS=false # Determines if all N robots have to agree or
+		       # only the beneficial subswarm.
+
+# Iterate over experimental settings and start experiments 
+. ./run_experiment.sh
